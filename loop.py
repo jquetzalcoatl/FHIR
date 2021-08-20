e96ffad648b4bcfe993abf0c72cea6c7d4083ad8
Javier Toledo <Javier.Toledo1@bcchr.ca>
2021-08-20 14:27:02 -0700

    loop

---
+++ loop.py
@@ -0,0 +1,24 @@
+from utils.observations import ObsDF
+from utils.dataAnalysis import dataObject
+import argparse
+import os
+from datetime import datetime, date, timedelta
+import time
+import logging
+
+# os.getcwd()
+# os.chdir(os.path.join(os.getcwd(), "FHIR"))
+
+if __name__ == '__main__':
+	while True:
+		print(f'Main - Current directory: {os.getcwd()}')
+		since = str(datetime.today() - timedelta(days=1)).split(" ")[0]
+		print(f'Main - Data transfer since {since} starting...')
+
+		r = ObsDF(date=since, MAX=0)
+		r.logging.info(f'{str(datetime.now()).split(".")[0]} - Bulk Export ended after t = {str(datetime.now() - r.initTime)}')
+		r.logging.info(f'#############################################################################################')
+		b = dataObject(since=since, aggregate=True)
+		time.sleep(86400) #Sleep 2*24hrs
+	# os.system(f'streamlit run visualization.py -- --date {str(datetime.now()).split(" ")[0]}')
+	# !streamlit run visualization.py -- --date str(datetime.now()).split(" ")[0]
